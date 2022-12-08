e56b014e4187cd5850c70fac04d6bcef309ae2fa
rush1001 <rushabh@craftcodersinfotech.com>
2022-12-07 22:59:17 -0800

    Initial commit

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { GoogleSignin, statusCodes } from "@react-native-google-signin/google-signin"
 
 const socialMedia = () => {
+  useEffect(() => {
+    GoogleSignin.configure()
+  }, [])
+  
+  const googleSignin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log("user info",userInfo);
+      
+    } catch (error:any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log(error);
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log(error);
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log(error);
+      } else {
+        console.log(error);
+      }
+    }
+  };
   return (
-    <View>
-      <Text>Index</Text>
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <TouchableOpacity style={{height:35,backgroundColor:"#cdcdcd",alignItems:"center",justifyContent:"center",width:100}}
+      onPress={()=>googleSignin()}>
+      <Text>Google SignIn</Text>
+      </TouchableOpacity>
     </View>
   )
 }
