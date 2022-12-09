7b004f78035187dc3c33d456b13731d064571c3f
rush1001 <rushabh@craftcodersinfotech.com>
2022-12-09 18:00:00 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
 # social-media-login-component
+
+<b>social-media-login</b> library provides an interface to providers with support for the following providers for React Native apps:
+
+1.Google
+2.Facebook
+3.Apple
+ 
+
+## Table of Content
+
+- [Features](#features)
+- [Installation](#installation)
+- [API reference](#api-reference)
+- [Google_signIn](#Google_signIn)
+- [Facebook_signIn](#Facebook_signIn)
+- [Apple_signIn](#Apple_signIn)
+
+## Features
+
+- Works for different platforms like Google, Facebook and Apple
+- Easy to access SignIn inside React component
+- Easy to use elements as other React Native elements
+
+## Installation
+
+To use this specific component first you will need to setup [firebase](https://rnfirebase.io/) into your project
+you can take reference from given link
+```
+https://rnfirebase.io/
+```
+
+After setting up Firebase , you must set up for [google-signin](https://rnfirebase.io/auth/social-auth), You can reference from given link
+```
+https://rnfirebase.io/auth/social-auth
+```
+
+To add the social-media-login-component to React Native app, run this command with tag version
+
+```
+npm install https://github.com/CraftCodersInfotech/social-media-login-component.git
+```
+
+Make sure to install pod into react-native Project after linking given command
+
+## API reference
+
+### SocialMedia
+
+| Name                  | Type              | Description                      |
+| :-------------------- | :---------------- | :------------------------------- |
+| size                 | number (Required) | size of checkbox                  |
+| unbackgroundColor | string          | Color of checkbox when not check |
+| selectedbackgroundColor | string            | Color of checkbox while check  |
+| label        | string     | label of checkbox             |
+| labelStyle             | ViewStyle     | Style of checkbox label          |
+| tintColor             | string         | color of checkbox image          |
+| value        | boolean     | The value of the checkbox. If true the checkbox will be turned on. Default value is false.|
+| labelDirectionLeft        | boolean     |  Direction of checkbox labeltext|
+| labelDirectionTop             | boolean     | Direction of checkbox labeltext          |
+
+## Example
+
+```javascript
+import { StyleSheet, View } from 'react-native'
+import React  from 'react'
+import SocialMedia from 'social-media-login'
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+
+const App = () => {
+  GoogleSignin.configure({
+     webClientId: "xxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com",    
+     //You can get werClientId from android/app/google-service.json
+  });
+   
+  return (
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <SocialMedia/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
+
+```
+
+
--- src/Index.tsx
+++ src/Index.tsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useContext, useEffect } from 'react'
 import { GoogleSignin, statusCodes } from "@react-native-google-signin/google-signin"
 import { SigninContext } from "./context"
+import auth from '@react-native-firebase/auth';
+import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
 
 const socialMedia = () => {
   const { registerUser, userData } = useContext<any>(SigninContext);
@@ -10,27 +12,66 @@ const socialMedia = () => {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
       await registerUser(userInfo);
-      console.log("user info",userInfo);
-      
-    } catch (error:any) {
+      console.log("user info", userInfo);
+
+    } catch (error: any) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log("error1",error);
+        console.log("error1", error);
       } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log("error2",error);
+        console.log("error2", error);
       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log("error3",error);
+        console.log("error3", error);
       } else {
-        console.log("error4",error);
+        console.log("error4", error);
       }
     }
   };
+
+  const faceBookSignin = async () => {
+    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+
+    const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+
+    return auth().signInWithCredential(facebookCredential);
+  }
+
   return (
-    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-      <TouchableOpacity style={{height:35,backgroundColor:"#cdcdcd",alignItems:"center",justifyContent:"center",width:100}}
-      onPress={()=>googleSignin()}>
-      <Text>Google SignIn</Text>
+    <>
+      <TouchableOpacity style={{ marginVertical: 12, paddingVertical: 10, backgroundColor: "#cdcdcd", alignItems: "center", justifyContent: "center", width: "100%", flexDirection: "row",borderRadius:8 }}
+        onPress={() => googleSignin()}>
+        <View style={{ marginRight: 15 }}>
+
+          <Image
+            style={{ height: 20, width: 20 }}
+            source={require('../src/assets/google.png')}
+          />
+        </View>
+        <Text>Google SignIn</Text>
       </TouchableOpacity>
-    </View>
+
+      <TouchableOpacity style={{ marginVertical: 12, paddingVertical: 10, backgroundColor: "#cdcdcd", alignItems: "center", justifyContent: "center", width: "100%", flexDirection: "row",borderRadius:8 }}
+        onPress={() => faceBookSignin()}
+      >
+        <View style={{ marginRight: 15 }}>
+
+          <Image
+            style={{ height: 20, width: 20 }}
+            source={require('../src/assets/FB.png')}
+          />
+        </View>
+        <Text>FaceBook SignIn</Text>
+      </TouchableOpacity>
+    </>
+
   )
 }
 
